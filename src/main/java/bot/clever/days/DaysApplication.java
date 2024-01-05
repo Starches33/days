0d6324d610444be81192ab2e8b903c47306877ce
Starina <Starches33@gmail.com>
2024-01-05 11:30:17 +0400

    first init

---
+++ src/main/java/bot/clever/days/DaysApplication.java
@@ -0,0 +1,13 @@
+package bot.clever.days;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaysApplication.class, args);
+	}
+
+}
